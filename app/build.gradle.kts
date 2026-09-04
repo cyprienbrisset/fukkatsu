68b5bf858ad40c92a864643008cdbe4b0be64584
cyprienbrisset <contact@cyprienbrisset.fr>
2026-09-04 18:21:18 +0200

    feat: TileRepository with add/reorder + instrumented test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,4 +65,7 @@ dependencies {
     testImplementation(libs.junit)
     testImplementation(libs.kotlinx.coroutines.test)
     androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.test.core)
+    androidTestImplementation(libs.androidx.room.testing)
+    androidTestImplementation(libs.kotlinx.coroutines.test)
 }
